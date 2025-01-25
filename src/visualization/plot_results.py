4396de3b5047496bfe1cded766b44c6da95bd826
Ahmed <a7mdixhd@gmail.com>
2025-01-25 12:41:44 +0300

    Update plot pipeline

--- src/visualization/plot_results.py
+++ src/visualization/plot_results.py
@@ -1,38 +1,48 @@
 import matplotlib.pyplot as plt
 import cv2
 
-def plot_pipeline(original_frame, face_region, left_eye_region, right_eye_region, mouth_region, decision):
+def plot_pipeline(original_frame, face_region,facial_features, states,decision):
+    
+    class_names_eye = {0: "Close_Eye", 1: "Open_Eye"}
+    class_names_mouth = {0: "No_Yawn", 1: "Yawn"}
     # Create a figure with subplots
     fig, axs = plt.subplots(1, 5, figsize=(20, 5))
 
     # Plot original frame
-    axs[0].imshow(cv2.cvtColor(original_frame, cv2.COLOR_BGR2RGB))
+    axs[0].imshow(original_frame)
     axs[0].set_title("Original Frame")
     axs[0].axis('off')
     # plt.show()
     # Plot face region
    
-    axs[1].imshow(cv2.cvtColor(face_region, cv2.COLOR_BGR2RGB))
+    axs[1].imshow(face_region)
     axs[1].set_title("Detected Face")
     axs[1].axis('off')
 
     # Plot left eye region
-    axs[2].imshow(cv2.cvtColor(left_eye_region, cv2.COLOR_BGR2RGB))
+    axs[2].imshow(facial_features.left_eye)
     axs[2].set_title("Extracted Left Eye")
     axs[2].axis('off')
     
     # Plot right eye region
-    axs[3].imshow(cv2.cvtColor(right_eye_region, cv2.COLOR_BGR2RGB))
+    axs[3].imshow(facial_features.right_eye)
     axs[3].set_title("Extracted Right Eye")
     axs[3].axis('off')
 
     # Plot mouth region
-    axs[4].imshow(cv2.cvtColor(mouth_region, cv2.COLOR_BGR2RGB))
+    axs[4].imshow(facial_features.mouth)
     axs[4].set_title("Extracted Mouth")
     axs[4].axis('off')
 
     # Show decision
-    plt.suptitle(f"Decision: {decision}", fontsize=16)
+    decision_as_text = (
+                        f"is_Drowsy: {decision.is_drowsy} ({decision.confidence:.2f})\n"
+                        f"Left Eye: {class_names_eye[states.left_eye_state]} ({states.confidence_left:.2f}), "
+                        f"Right Eye: {class_names_eye[states.right_eye_state]} ({states.confidence_right:.2f}), "
+                        f"Mouth: {class_names_mouth[states.mouth_state]} ({states.confidence_mouth:.2f})"
+                    )
+
+    plt.suptitle(decision_as_text, fontsize=16)
 
     # Display the plot
     plt.show()
