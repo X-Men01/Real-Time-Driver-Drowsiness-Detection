bba090ec0dfcd6bf6d82616f7170865ae62d90eb
Ahmed <a7mdixhd@gmail.com>
2025-01-30 09:25:37 +0300

    add model V2

--- src/models/cnn_model.py
+++ src/models/cnn_model.py
@@ -53,3 +53,60 @@ class Custom_CNN(nn.Module):
         # print(x.shape)
         x = self.classifier(x)
         return x
+    
+    
+class Custom_CNN_V1(nn.Module):
+    def __init__(self):
+        super(Custom_CNN_V1, self).__init__()
+        self.input_shape = 3
+        self.hidden_units = 10
+        self.output_shape = 2
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=self.input_shape,out_channels=self.hidden_units,kernel_size=3,stride=1,padding=1,),
+            nn.BatchNorm2d(self.hidden_units),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=self.hidden_units,out_channels=self.hidden_units,kernel_size=3,stride=1,padding=1,),
+            nn.BatchNorm2d(self.hidden_units),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),)
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=self.hidden_units,out_channels=self.hidden_units * 2,kernel_size=3,stride=1,padding=1,),
+            nn.BatchNorm2d(self.hidden_units * 2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=self.hidden_units * 2,out_channels=self.hidden_units * 2,kernel_size=3,stride=1,padding=1,),
+            nn.BatchNorm2d(self.hidden_units * 2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.conv_block_3 = nn.Sequential(
+            nn.Conv2d(in_channels=self.hidden_units * 2,out_channels=self.hidden_units * 4,kernel_size=3,stride=1,padding=1,),
+            nn.BatchNorm2d(self.hidden_units * 4),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=self.hidden_units * 4,out_channels=self.hidden_units * 4,kernel_size=3,stride=1,padding=1,),
+            nn.BatchNorm2d(self.hidden_units * 4),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), )
+        self.conv_block_4 = nn.Sequential(
+            nn.Conv2d(in_channels=self.hidden_units * 4,out_channels=self.hidden_units * 4,kernel_size=3,stride=1,padding=1,),
+            nn.BatchNorm2d(self.hidden_units * 4),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=self.hidden_units * 4,out_channels=self.hidden_units * 4,kernel_size=3,stride=1,padding=1,),
+            nn.BatchNorm2d(self.hidden_units * 4),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), )
+
+        self.classifier = nn.Sequential(
+
+            nn.Flatten(),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=self.hidden_units *8*8 *4, out_features=self.output_shape),
+        )
+
+    def forward(self, x: torch.Tensor):
+        # print(x.shape)
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.conv_block_3(x)
+        x = self.conv_block_4(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        return x
