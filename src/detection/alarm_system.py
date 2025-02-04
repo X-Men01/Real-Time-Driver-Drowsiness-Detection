614d1658ee4b471f855fc882158e110aaf2d31bf
Ahmed <a7mdixhd@gmail.com>
2025-02-04 18:49:09 +0300

    Update alarm_system.py

--- src/detection/alarm_system.py
+++ src/detection/alarm_system.py
@@ -1,9 +1,25 @@
 import playsound
+import threading
 from config import Config
 
 class AlarmSystem:
     def __init__(self,config: Config):
-        self.alarm_file = config.ALARM_FILE
+        self.alarm_files = {
+            "Drowsiness": config.DROWSINESS_ALARM_FILE,
+            "Distraction": config.DISTRACTION_ALARM_FILE
+        }
+        self.alarm_thread = None
 
-    def trigger_alarm(self):
-        playsound.playsound(self.alarm_file)
+    def _play_alarm(self,alarm_type: str):
+        playsound.playsound(self.alarm_files[alarm_type])
+
+    def trigger_alarm(self , alarm_type: str):
+        if self.alarm_thread and self.alarm_thread.is_alive():
+            return
+    
+        self.alarm_thread = threading.Thread(target=self._play_alarm, daemon=True, args=(alarm_type,))
+        self.alarm_thread.start()
+        
+    @property
+    def is_active(self):
+        return self.alarm_thread is not None and self.alarm_thread.is_alive()   
