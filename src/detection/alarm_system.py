9b8fba15d543b7e4e1d7156c54b81950a4daa225
Ahmed <a7mdixhd@gmail.com>
2025-02-18 19:05:41 +0300

    Add alarm_type

--- src/detection/alarm_system.py
+++ src/detection/alarm_system.py
@@ -10,17 +10,19 @@ class AlarmSystem:
             "Face_not_detected": config.FACE_NOT_DETECTED_ALARM_FILE
         }
         self.alarm_thread = None
-
+        self.alarm_type = None
     def _play_alarm(self,alarm_type: str):
         playsound.playsound(self.alarm_files[alarm_type])
 
     def trigger_alarm(self , alarm_type: str):
         if self.alarm_thread and self.alarm_thread.is_alive():
             return
-    
+        self.alarm_type = alarm_type
         self.alarm_thread = threading.Thread(target=self._play_alarm, daemon=True, args=(alarm_type,))
         self.alarm_thread.start()
         
     @property
     def is_active(self):
-        return self.alarm_thread is not None and self.alarm_thread.is_alive()   
+        return self.alarm_thread is not None and self.alarm_thread.is_alive()    
+    
+ 
